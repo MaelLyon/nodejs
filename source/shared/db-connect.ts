6a4cbebdba75af8d9279f089ecd66653d789ab65
mael Pillon <mpillon@impactbuying>
2023-09-29 09:49:25 +0200

    Fix init (db still not configured)

--- source/shared/db-connect.ts
+++ source/shared/db-connect.ts
@@ -1,5 +1,5 @@
 import mysql from "mysql2";
-import { dbConfig } from "../../config/db.config";
+import { dbConfig } from "../config/db.config";
 
 export default mysql.createConnection({
   host: dbConfig.HOST,
