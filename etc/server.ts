977eeead89c732ba959f1d734d5a87d581430834
Mael <pillonmael6966@gmail.com>
2023-10-03 17:30:26 +0200

    try fix render

--- etc/server.ts
+++ etc/server.ts
@@ -1,32 +1,9 @@
 /** source/server.ts */
 import http from 'http';
-import express, { Express } from 'express';
-import morgan from 'morgan';
+import express from 'express';
 import routes from './routes/test';
 
-const router: Express = express();
-
-/** Logging */
-router.use(morgan('dev'));
-/** Parse the request */
-router.use(express.urlencoded({ extended: false }));
-/** Takes care of JSON data */
-router.use(express.json());
-
-/** RULES OF OUR API */
-router.use((req, res, next) => {
-    // set the CORS policy
-    res.header('Access-Control-Allow-Origin', '*');
-    // set the CORS headers
-    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-    // set the CORS method headers
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
-        return res.status(200).json({});
-    }
-    next();
-});
-
+const router = express();
 /** Routes */
 router.use('/', routes);
 
@@ -39,6 +16,8 @@ router.use((req, res, next) => {
 });
 
 /** Server */
-const httpServer = http.createServer(router);
 const PORT: any = process.env.PORT || 3001;
-httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+const httpServer = router.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+
+httpServer.keepAliveTimeout = 120 * 1000;
+httpServer.headersTimeout = 120 * 1000;
