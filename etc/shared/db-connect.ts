793fda42202e8af7a02b0fe04be727f988aa8e4b
Mael <pillonmael6966@gmail.com>
2023-10-03 12:07:03 +0200

    use process.env

--- etc/shared/db-connect.ts
+++ etc/shared/db-connect.ts
@@ -1,9 +1,8 @@
 import mysql from "mysql2";
-import { dbConfig } from "../secrets/db.config";
 
 export default mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
