577912e7053144fc833d5aaa08032ab7ae705dfe
Mael <pillonmael6966@gmail.com>
2023-10-03 12:47:27 +0200

    Change port

--- etc/server.ts
+++ etc/server.ts
@@ -40,5 +40,5 @@ router.use((req, res, next) => {
 
 /** Server */
 const httpServer = http.createServer(router);
-const PORT: any = process.env.PORT ?? 6060;
+const PORT: any = process.env.APP_PORT ?? 6060;
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
